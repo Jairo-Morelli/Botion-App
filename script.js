c9c528b5e0e7eeaa610d942b75f49f4ff5846345
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-05-13 22:43:42 -0400

    Finished addhoverInfo feature

--- script.js
+++ script.js
@@ -6,6 +6,10 @@
 
 
 
+let cardJSONArray; 
+let DashboardJson;
+
+
 /*
 Just a note that I want to bring up. Depending on where I inject 
 my javascript code in the html file, this code below will either run before 
@@ -18,6 +22,29 @@ const addHabitButton = document.getElementById("btn-add");
 //works
 addHabitButton.addEventListener("mouseover", ()=>
 {
-    console.log("Mouse over");
-    console.log(addHabitButton.style);
+    //Access div container through .html .css attribute
+    const addDivElements = document.getElementsByClassName("Btn");
+
+    /*This is some pretty hard coded code right here. I am 
+    taking the approach that if I already know the div structure, 
+    therefore I will always be able to accessing my add div structure
+    and then access my addHoverInfo text.
+
+    This doesn't feel right, but this is the design choice I am making.
+    */
+    const addHoverInfo = addDivElements[0].children[0];
+
+    //First way I decided to do it
+    //addHoverInfo.style.visibility="visible";
+    //I perfer working with numbers.
+    addHoverInfo.style.opacity=1.0;
+})
+
+addHabitButton.addEventListener("mouseleave", ()=>{
+
+    const addDivElements= document.getElementsByClassName("Btn");
+
+    const addHoverInfo = addDivElements[0].children[0];
+
+    addHoverInfo.style.opacity=0.0;
 })

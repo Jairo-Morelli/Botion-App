56407d3b35f146bf2efabee71f9e236fcb0f7bc4
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-07-09 20:26:45 -0400

    Finished Nav Bar

--- Flash-Card-App/src/Components/Navigation.jsx
+++ Flash-Card-App/src/Components/Navigation.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-function Navigation({props}) 
-{
-    return(
-        <nav id="menu" className="navbar navbar-default navbar-fixed-top">
-            <div>
-            <ul className="text-white flex justify-evenly list-none p-o m-0 w-full text-[1.5rem]">
-                <li className="inline slide-under-anim font-[Montserrat]">Home</li>
-                <li className="inline slide-under-anim font-[Montserrat]">About</li>
-                <li className="inline slide-under-anim font-[Montserrat]">Contact</li>
-            </ul>
+function Navigation({ props }) {
+    return (
+        <>
+            <div className=" border-b border-white w-[250%] ml-[-68.5%]">
+                <nav id="menu" className="navbar navbar-default navbar-fixed-top ">
+                    <div className="before:">
+                        <div className="navbar-header inline-block w-[300px]">
+                            <h2 className="text-4xl text-white font-[Montserrat] uppercase p-[5px] mr-[600px]">botion</h2>
+                        </div>
+                        <button className="inline slide-under-anim font-[Montserrat] text-white p-[20px] text-2xl">Home</button>
+                        <button className="inline slide-under-anim font-[Montserrat] text-white p-[20px] text-2xl">About</button>
+                        <button className="inline slide-under-anim font-[Montserrat] text-white p-[20px] text-2xl">Contact</button>
+                    </div>
+                </nav>
             </div>
-        </nav>
+        </>
     )
 }
 

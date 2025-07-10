aaa1d81fdf219cc7ac53c9c40c24b1c69d42a15a
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-07-09 21:32:14 -0400

    added Library Icon

--- Flash-Card-App/src/Components/Navigation.jsx
+++ Flash-Card-App/src/Components/Navigation.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
+
 function Navigation({ props }) {
     return (
         <>
             <div className=" border-b border-white w-[250%] ml-[-68.5%]">
                 <nav id="menu" className="navbar navbar-default navbar-fixed-top ">
                     <div className="before:">
+                    <img className="inline-block"src="./../../public/library_Icon.svg" />
                         <div className="navbar-header inline-block w-[300px]">
                             <h2 className="text-4xl text-white font-[Montserrat] uppercase p-[5px] mr-[600px]">botion</h2>
                         </div>

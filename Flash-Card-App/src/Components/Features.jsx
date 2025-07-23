5b9cf8e7c28ce5f618336eda33d6b6c06fb2d29a
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-07-23 15:52:37 -0400

    added images to my feature bar

--- Flash-Card-App/src/Components/Features.jsx
+++ Flash-Card-App/src/Components/Features.jsx
@@ -1,21 +1,30 @@
 import React, { useState } from "react"
 import styles from "./../Modules/HeroSection.module.css"
 
-function Features({props}) {
+function Features({ props }) {
     return (
         <>
             <div className={styles.sectiontitle}>
                 <div className="@container">
                     { /* Building my webpages with boxes, using flex/grid.*/}
-                    <div className="col-md-10 col-md-offset-1 section-title">
+                    <div className="">
                         <h1 className={styles.featuretitle}>Features</h1>
-                        <div className="row">
-                            { 
-                             props && Array.isArray(props) && props.map((currentBook)=>{
-                                    return <li>{currentBook.title}</li>
-                             })
+                        <div className="flex flex-row justify-between">
+                            {
+
+                                props && Array.isArray(props) && props.map((currentBook, index) => {
+                                    return(
+                                    <li key={index} className="md:p-4 md:m-0.5">{currentBook.title}
+                                    {currentBook.icon && (
+                                        <>
+                                        <img src="../../public/Book.svg"></img>
+                                        </>
+                                    )}
+                                    </li>
+                                    )
+                                })
                             }
-                        </div>
+                        </div >
                     </div>
                 </div>
             </div>

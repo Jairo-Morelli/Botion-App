9529c1a797582a85b9ec312541390060a510f5a3
Jairo411 <40253573+Jairo411@users.noreply.github.com>
2025-07-22 21:41:19 -0400

    Wrote code in Feature Component

--- Flash-Card-App/src/App.jsx
+++ Flash-Card-App/src/App.jsx
@@ -1,21 +1,25 @@
-import { useEffect, useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PlainApp from "./plainApp"
-import HeroSection from "./Components/HeroSection"
-import Navigation from './Components/Navigation'
-import Features from "./Components/Features"
-import ContactPage from "./Pages/Contact"
-import AboutPage from "./Pages/About"
-import styles from "./Modules/HeroSection.module.css"
-
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PlainApp from "./plainApp";
+import HeroSection from "./Components/HeroSection";
+import Navigation from "./Components/Navigation";
+import Features from "./Components/Features";
+import ContactPage from "./Pages/Contact";
+import AboutPage from "./Pages/About";
+import styles from "./Modules/HeroSection.module.css";
+import self_development_books from "./data/self_development_books.json";
+import septhen_king_books from "./data/stephen_king_books.json";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [featureCardData, setFeatureCardData] = useState({});
 
   useEffect(() => {
-
+    setFeatureCardData([...self_development_books]);
+    setFeatureCardData(prev => [...prev,...septhen_king_books]);
+    console.log(featureCardData);
   }, []);
 
   return (
@@ -31,8 +35,8 @@ function App() {
         <div className={styles.background}>
         <HeroSection />
         </div>
-        <div className='w-[100%] '>
-        <Features />
+        <div className='w-[100%]'>
+        <Features props={featureCardData} />
         </div>
       </div>
     </>
--- Flash-Card-App/src/Components/Features.jsx
+++ Flash-Card-App/src/Components/Features.jsx
@@ -1,11 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./../Modules/HeroSection.module.css"
 
-function Features() {
+function Features({props}) {
     return (
-        <>               
+        <>
             <div className={styles.sectiontitle}>
-                <h1 className={styles.title}>Features</h1>
+                <div className="@container">
+                    { /* Building my webpages with boxes, using flex/grid.*/}
+                    <div className="col-md-10 col-md-offset-1 section-title">
+                        <h1 className={styles.featuretitle}>Features</h1>
+                        <div className="row">
+                            {
+                                 props.map((currentBook) => {
+                                     return <li>{currentBook.title}</li>
+                                 })
+                            }
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
